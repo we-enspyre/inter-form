d4b6f8128b1c5614a7eb7a9e7a1049f01b10090d
MHA <98241106+MHA963@users.noreply.github.com>
2025-09-26 16:11:04 +0200

    basename

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: mode === "development" ? "/" : "/inter-form/",
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
